639ac35b068e902f888d32fa1ada1296a4964a53
nikhil kaki <aryu14@gmail.com>
2025-03-09 01:13:52 +0530

    feat:added signup functionality

--- auth-service/controller/user.ts
+++ auth-service/controller/user.ts
@@ -1,26 +1,84 @@
-import { failureResponse, successResponse } from "../helpers/responseHelpers";
-import { HTTP_STATUS_CODE } from "../helpers/constants";
-
-
+import { failureResponse, successResponse } from '../helpers/responseHelpers';
+import { HTTP_STATUS_CODE } from '../helpers/constants';
+import { validationResult, body } from 'express-validator';
 import { Request, Response } from 'express';
 import { loginUser, refreshToken } from '../services/authService';
 import { redisClient } from '../db/connectRedis';
+import { User } from '../models/user';
+import bcrypt from 'bcrypt';
 
+export const signUpValidation = [
+  body('providerType').isIn(['individual', 'company']).notEmpty(),
+  body('firstName').isString().notEmpty(),
+  body('lastName').isString().notEmpty(),
+  body('email').isEmail().notEmpty(),
+  body('password')
+    .isLength({ min: 8 })
+    .matches(/[A-Z]/)
+    .matches(/[a-z]/)
+    .matches(/\d/)
+    .matches(/[!@#$%^&*(),.?":{}|<>]/),
+  body('companyName').if(body('providerType').equals('company')).isString().notEmpty(),
+  body('businessTaxNumber')
+    .if(body('providerType').equals('company'))
+    .matches(/^[A-Z0-9]{10}$/),
+  body('mobileNumber').isString().notEmpty(),
+  body('streetNumber').if(body('providerType').equals('company')).isString().notEmpty(),
+  body('streetName').if(body('providerType').equals('company')).isString().notEmpty(),
+  body('city').if(body('providerType').equals('company')).isString().notEmpty(),
+  body('state').if(body('providerType').equals('company')).isString().notEmpty(),
+  body('postCode').if(body('providerType').equals('company')).isString().notEmpty(),
+];
+
+export const signUp = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return failureResponse(res, HTTP_STATUS_CODE.BAD_REQUEST, errors.array());
+  }
+
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      return failureResponse(res, HTTP_STATUS_CODE.BAD_REQUEST, 'Email already exists');
+    }
+
+    req.body.passwordHash = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create(req.body);
+
+    return successResponse(res, HTTP_STATUS_CODE.CREATED, { userId: newUser.id, email: newUser.email });
+  } catch (err: any) {
+    return failureResponse(res, HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR, err.message);
+  }
+};
+
+export const loginValidation = [
+  body('email').isEmail().notEmpty(),
+  body('password')
+    .isLength({ min: 8 })
+    .matches(/[A-Z]/)
+    .matches(/[a-z]/)
+    .matches(/\d/)
+    .matches(/[!@#$%^&*(),.?":{}|<>]/),
+];
 export const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
     const tokens = await loginUser(email, password);
     res.json(tokens);
-  } catch (err:any) {
+  } catch (err: any) {
     return failureResponse(res, HTTP_STATUS_CODE.UNAUTHORIZED, err.message);
   }
 };
 
+export const refreshTokenValidation = [body('refreshToken').isString().notEmpty()];
 export const refresh = async (req: Request, res: Response) => {
   try {
     const tokens = await refreshToken(req.body.refreshToken);
     res.json(tokens);
-  } catch (err:any) {
+  } catch (err: any) {
     return failureResponse(res, HTTP_STATUS_CODE.ACCESS_FORBIDDEN, err.message);
   }
 };
--- auth-service/routes/api/user.ts
+++ auth-service/routes/api/user.ts
@@ -1,27 +1,34 @@
 import express, { Request, Response } from 'express';
 import { failureResponse } from '../../helpers/responseHelpers';
-import { login, logout, refresh } from '../../controller/user';
+import { login, logout, refresh, signUp, signUpValidation, loginValidation, refreshTokenValidation } from '../../controller/user';
 import { HTTP_STATUS_CODE } from '../../helpers/constants';
 
 const router = express.Router();
 
 router
+  .route('/signup')
+  .post(signUpValidation, signUp)
+  .all((req: Request, res: Response) => {
+    return failureResponse(res, HTTP_STATUS_CODE.METHOD_NOT_ALLOWED, 'METHOD_NOT_ALLOWED');
+  });
+
+router
   .route('/login')
-  .post(login)
+  .post(loginValidation, login)
   .all((req: Request, res: Response) => {
     return failureResponse(res, HTTP_STATUS_CODE.METHOD_NOT_ALLOWED, 'METHOD_NOT_ALLOWED');
   });
 
 router
   .route('/refresh')
-  .post(refresh)
+  .post(refreshTokenValidation, refresh)
   .all((req: Request, res: Response) => {
     return failureResponse(res, HTTP_STATUS_CODE.METHOD_NOT_ALLOWED, 'METHOD_NOT_ALLOWED');
   });
 
 router
   .route('/logout')
-  .post(logout)
+  .post(refreshTokenValidation, logout)
   .all((req: Request, res: Response) => {
     return failureResponse(res, HTTP_STATUS_CODE.METHOD_NOT_ALLOWED, 'METHOD_NOT_ALLOWED');
   });
