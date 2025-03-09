caa5c8d186a720b729b919b46f75479028a14630
nikhil kaki <aryu14@gmail.com>
2025-03-09 23:50:34 +0530

    fix:filename issue in offer-service

--- offer-service/index.ts
+++ offer-service/index.ts
@@ -10,6 +10,10 @@ import { HTTP_STATUS_CODE } from './helpers/constants';
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PROTO_PATH = path.join(__dirname, './proto/task.proto');
 
